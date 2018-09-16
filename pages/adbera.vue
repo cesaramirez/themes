c12a7d7f8509cea7a7e85afb5dbbddf461b9dabd
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2018-09-16 00:17:53 -0600

    :sparkles: Start markup for theme adbera

--- pages/adbera.vue
+++ pages/adbera.vue
@@ -1,6 +1,28 @@
 <template>
-  <div class="bg-blue h-screen flex items-center justify-center">
-    <h1 class="text-white ">Adbera</h1>
+  <div class="h-full">
+    <header class="bg-green">
+      <nav class="flex justify-between items-center container mx-auto pt-8 pb-32">
+        <div class="text-white text-3xl">
+          <img src="@/assets/logo.svg" alt="Logo image" class="h-12">
+        </div>
+        <a href="#"
+           class="px-6 h-10 flex items-center rounded-full bg-green-dark hover:bg-green-darker hover:shadow text-base text-white no-underline">
+           Lorem Ipsum
+        </a>
+      </nav>
+    </header>
+    <section class="h-auto bg-green pb-12">
+      <div class="container mx-auto flex">
+        <div class="w-1/2">
+          <h1 class="font-bold text-5xl text-white mb-8">Lorem ipsum dolor sit amet consectetur</h1>
+          <p class="text-white leading-loose tracking-normal text-lg">Praesent mollis, massa in auctor molestie, magna lorem semper mauris, a venenatis turpis purus vel metus. Etiam sed iaculis libero, at euismod massa. Fusce in venenatis ligula.</p>
+        </div>
+        <div class="ml-auto">
+          <img src="@/assets/hero-image.svg" alt="Product image" class="h-80">
+        </div>
+      </div>
+    </section>
+    <section class="h-auto bg-grey-lighter py-12"></section>
   </div>
 </template>
 
