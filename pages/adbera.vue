4813263f60818fa34560c187ce3004d759b53195
César A. Ramírez <cesar.ramirez.sv@gmail.com>
2018-09-17 22:45:48 -0600

    :sparkles: Add one more section and improvement responsive

--- pages/adbera.vue
+++ pages/adbera.vue
@@ -12,17 +12,45 @@
       </nav>
     </header>
     <section class="h-auto bg-green pb-12">
-      <div class="container mx-auto flex">
-        <div class="w-1/2">
+      <div class="container mx-auto flex flex-col xl:flex-row">
+        <div class="w-full xl:w-1/2 text-center xl:text-left mb-6 xl:mb-0">
           <h1 class="font-bold text-5xl text-white mb-8">Lorem ipsum dolor sit amet consectetur</h1>
           <p class="text-white leading-loose tracking-normal text-lg">Praesent mollis, massa in auctor molestie, magna lorem semper mauris, a venenatis turpis purus vel metus. Etiam sed iaculis libero, at euismod massa. Fusce in venenatis ligula.</p>
         </div>
-        <div class="ml-auto">
-          <img src="@/assets/hero-image.svg" alt="Product image" class="h-80">
+        <div class="xl:ml-auto">
+          <img src="@/assets/hero-image.svg" alt="Product image" class="xl:h-80">
         </div>
       </div>
     </section>
-    <section class="h-auto bg-grey-lighter py-12"></section>
+    <section class="h-auto bg-grey-lighter py-12">
+      <div class="flex flex-col justify-center items-center mb-16">
+        <h2 class="mb-6 text-3xl">Lorem ipsum dolor</h2>
+        <p class="text-xl md:w-2/3 xl:w-1/2 text-center leading-normal tracking-wide text-grey-darker">Praesent mollis, massa in auctor molestie, magna lorem semper mauris, a venenatis turpis purus vel metus.</p>
+      </div>
+      <div class="flex flex-col xl:flex-row justify-center items-center">
+        <div class="bg-white md:w-2/3 xl:w-1/5 h-auto rounded-lg shadow-lg flex xl:flex-col items-center justify-center mx-4 py-10 hover:shadow-md mb-8 xl:mb-0">
+          <img src="@/assets/feature-grid-1.svg" alt="" class="h-32 xl:h-48 xl:mb-8 ml-8 xl:ml-0">
+          <div class="text-left xl:text-center px-6 w-2/3">
+            <h4 class="mb-4 text-2xl font-medium">Lorem Ipsum</h4>
+            <p class="leading-normal">In maximus libero sed leo vulputate pretium. In sit amet nisi ligula</p>
+          </div>
+        </div>
+        <div class="bg-white md:w-2/3 xl:w-1/5 h-auto rounded-lg shadow-lg flex xl:flex-col items-center justify-center mx-4 py-10 hover:shadow-md mb-8 xl:mb-0">
+          <img src="@/assets/feature-grid-2.svg" alt="" class="h-32 xl:h-48 xl:mb-8 ml-8 xl:ml-0">
+          <div class="text-left xl:text-center px-6 w-2/3">
+            <h4 class="mb-4 text-2xl font-medium">Lorem Ipsum</h4>
+            <p class="leading-normal">In maximus libero sed leo vulputate pretium. In sit amet nisi ligula</p>
+          </div>
+        </div>
+        <div class="bg-white md:w-2/3 xl:w-1/5 h-auto rounded-lg shadow-lg flex xl:flex-col items-center justify-center mx-4 py-10 hover:shadow-md mb-8 xl:mb-0">
+          <img src="@/assets/feature-grid-3.svg" alt="" class="h-32 xl:h-48 xl:mb-8 ml-8 xl:ml-0">
+          <div class="text-left xl:text-center px-6 w-2/3">
+            <h4 class="mb-4 text-2xl font-medium">Lorem Ipsum</h4>
+            <p class="leading-normal">In maximus libero sed leo vulputate pretium. In sit amet nisi ligula</p>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
